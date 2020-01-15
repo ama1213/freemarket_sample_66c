98dcfc5833bc315b7e62819aadbaa1d7d489d465
ama1213 <amasei1213@icloud.com>
2020-01-15 20:20:38 +0900

    show_order_test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
-  # root to: 'posts#menu'
-  root to: 'products#show'
+  get 'layouts/footer', to: 'layouts#footer'
+  get 'layouts/application', to: 'layouts#application'
+  get 'layouts/mailer', to: 'layouts#mailer'
+  get 'layouts/mailer.text', to: 'layouts#mailer.text'
+
+  get 'posts/footer', to: 'posts#footer'
+  get 'posts/header', to: 'posts#header'
+  get 'posts/login', to: 'posts#login'
+  get 'posts/product', to: 'posts#product'
+  get 'posts/index', to: 'posts#index'
+  get 'posts/menu', to: 'posts#menu'
+  get 'posts/index', to: 'posts#index'
+
+  get 'products/order', to: 'products#order'
+  get 'products/show', to: 'products#show'
+
+  get 'users/sidebar', to: 'users#sidebar'
+  get 'users/edit', to: 'users#edit'
+  get 'users/identity_information', to: 'users#identity_information'
+  get 'users/logout', to: 'users#logout'
+  get 'users/payment', to: 'users#payment'
+  get 'users/show', to: 'users#show'
 end
