0d0c9748757d23094a745a7bf36f362b178d6243
ama1213 <amasei1213@icloud.com>
2020-01-15 15:21:32 +0900

    test_show

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+class ProductsController < ApplicationController
+  def comment
+  end
+
+  def content
+  end
+
+  def order
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  resources :comments
   root to: 'products#show'
 end
