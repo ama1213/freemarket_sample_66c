ffad6b5ff26650773378fcbb04b6101dab75b1f6
ama1213 <amasei1213@icloud.com>
2020-01-20 16:50:38 +0900

    tako

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def  configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birthday_year, :birthday_month, :birthday_day, :phone_number, :kanji_family, :kanji_name, :katakana_family, :katakana_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:text, :post_number, :prefecture, :municipality, :house_number, :building_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birthday_year, :birthday_month, :birthday_day, :phone_number, :kanji_family, :kanji_name, :katakana_family, :katakana_name, :post_number1, :prefecture_id, :municipality1, :house_number1, :building_name1, :address_phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:text, :post_number2, :prefecture2, :municipality2, :house_number2, :building_name2])
   end
 end
--- db/migrate/20200120072454_rename_prefecture1_column_to_users.rb
+++ db/migrate/20200120072454_rename_prefecture1_column_to_users.rb
@@ -1,4 +1,4 @@
-class RenameTitleColumnToPrefecture1 < ActiveRecord::Migration[5.2]
+class RenamePrefecture1ColumnToUsers < ActiveRecord::Migration[5.2]
   def change
     rename_column :users, :prefecture1, :prefecture_id
   end
