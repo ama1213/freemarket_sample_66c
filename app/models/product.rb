39a0e3e49dc29554f3e3a2627989448bac5d6b87
taka0san <takashun722@gmail.com>
2020-01-27 19:24:07 +0900

    search edit

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
+
+  def self.search(search)
+    if search
+      Product.where(['name LIKE ?', "%#{search}%"])
+    else
+      Product.all
+    end
+  end
 end
