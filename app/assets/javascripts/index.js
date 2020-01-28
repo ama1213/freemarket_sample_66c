b7940c38c7aac0407eae05be0cf1970c0565a528
taka0san <takashun722@gmail.com>
2020-01-28 23:42:55 +0900

    search left create

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -9,4 +9,13 @@ $(function(){
     prevArrow: '<div class="slider-arrow slider-prev fa fa-angle-left"></div>',
     nextArrow: '<div class="slider-arrow slider-next fa fa-angle-right"></div>',
   });
+  $('.search__right__content__box__area__image').each(function(){
+    var img_height = $(this).height();
+    var img_width  = $(this).width();
+    if((img_width / img_height) >= 1){
+      $(this).css("height", "100%");
+    }else{
+      $(this).css("width", "100%");
+    }
+  });
 });
