c45c1c4b1311f2dc793eff20c4c8ae9e2511e2d9
taka0san <takashun722@gmail.com>
2020-01-28 15:57:45 +0900

    error chenge

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :user,foreign_key: 'user_id'
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :category
-  has_many :images, dependent: :destroy
 
   def self.search(search)
     if search
