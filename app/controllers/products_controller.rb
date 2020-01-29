7b1c4088da65acc1a9d885098ecc896289d22d62
ama1213 <amasei1213@icloud.com>
2020-01-29 22:54:46 +0900

    change_7

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,8 +41,6 @@ class ProductsController < ApplicationController
   end
 
   def order
-    card = Card.find_by(user_id: current_user.id).first
-    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
     if card.blank?
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "users/registrations", action: "new_card"
@@ -57,7 +55,6 @@ class ProductsController < ApplicationController
 
   def pay
     @product = Product.find(params[:product_id])
-    card = Card.find_by(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_ACCESS_KEY']
     Payjp::Charge.create(
     :amount: @product.price, #支払金額を入力（itemテーブル等に紐づけても良い）
