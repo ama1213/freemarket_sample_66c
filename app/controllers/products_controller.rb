4f695863e8ed2ac17297ee13b96f10fceb4e19c1
taka0san <takashun722@gmail.com>
2020-01-30 20:09:22 +0900

    aa

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:destroy, :show]
+  before_action :set_product, only: [:destroy, :show, :edit, :update]
   def index
     @products = Product.includes(:images).order('created_at DESC')
   end
