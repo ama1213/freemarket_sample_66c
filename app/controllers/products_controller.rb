825d6902bd9c27d21b19dcef0d04d4ce9277556b
ama1213 <amasei1213@icloud.com>
2020-01-30 12:35:24 +0900

    complete_change

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:destroy, :show,]
-  before_action :set_card, only: [:order, :pay]
   def index
     @products = Product.includes(:images).order('created_at DESC')
   end
@@ -31,9 +30,8 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    if @product.user_id == current_user.id && @product.destroy
-      redirect_to root_path
-    end
+    @product.destroy
+    redirect_to root_path
   end
 
   def show
@@ -41,6 +39,8 @@ class ProductsController < ApplicationController
   end
 
   def order
+    @product = Product.find(params[:product_id])
+    card = Card.where(user_id: current_user.id).first
     if card.blank?
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "users/registrations", action: "new_card"
@@ -55,11 +55,12 @@ class ProductsController < ApplicationController
 
   def pay
     @product = Product.find(params[:product_id])
+    card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_ACCESS_KEY']
     Payjp::Charge.create(
-    :amount: @product.price, #支払金額を入力（itemテーブル等に紐づけても良い）
-    :customer: card.customer_id, #顧客ID
-    :currency: 'jpy', #日本円
+    :amount => @product.price, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :customer => card.customer_id, #顧客ID
+    :currency => 'jpy', #日本円
     ) #完了画面に移動
     redirect_to product_complete_path(params[:product_id])
   end
@@ -75,10 +76,6 @@ class ProductsController < ApplicationController
 
   def set_product
     @product = Product.find(params[:id])
-  end
-
-  def set_card
-    @card = Card.find_by(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
   end
 
   private
--- db/migrate/20200129063055_rename_prefecture1_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenamePrefecture1ColumnToUsers < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :users, :prefecture1, :prefecture_id
-  end
-end
